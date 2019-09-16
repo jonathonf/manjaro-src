d357b8b3555d8f04966cd8e75f7f1a5735be1553
Jonathon Fernyhough <jonathon@manjaro.org>
2019-09-16 16:25:07 +0100

    Set $PWD as build dir

--- makesrcpackage.sh
+++ makesrcpackage.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 cd /build
 mkdir sources srcpackages makepkglogs || true
-chown -R builder /home/builder
+chown -R builder /home/builder /builds
 
 echo "Building source package..."
 sudo -u builder script -q -c "/usr/bin/makepkg --noconfirm --noprogressbar --nosign -CSc --allsource" /dev/null
